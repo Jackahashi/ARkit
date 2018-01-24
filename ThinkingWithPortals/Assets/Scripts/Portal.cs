f4072de1fafcea680f6b14f66db5484715518634
JackQuick <jack@startvr.co>
2018-01-24 16:50:22 +1100

    Working with unity 2017.3 single AR portal

--- ThinkingWithPortals/Assets/Scripts/Portal.cs
+++ ThinkingWithPortals/Assets/Scripts/Portal.cs
@@ -6,6 +6,9 @@ public class Portal : MonoBehaviour {
 	[Header("Cameras")]
 	public Camera mainCam;
 	public Camera portalCam;
+    public string arworld1;
+   // public float coolDown = 1.0f;
+   // private bool portalActive = true;
 
 	void Update () {
 
@@ -19,10 +22,18 @@ public class Portal : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
 		if (other.CompareTag ("MainCamera")) {
-
-			// Use xor operator to toggle the ARWorld layer in the mainCam's culling mask.
-			mainCam.cullingMask ^= 1 << LayerMask.NameToLayer("ARWorld");
-
+            // Use xor operator to toggle the ARWorld layer in the mainCam's culling mask.
+            mainCam.cullingMask ^= 1 << LayerMask.NameToLayer(arworld1);
+            portalCam.cullingMask ^= 1 << LayerMask.NameToLayer(arworld1);
+           // portalActive = false;
+           // StartCoroutine(CoolDown());
 		}
 	}
+
+  //  IEnumerator CoolDown()
+  //  {
+  //     yield return new WaitForSeconds(coolDown);
+  //     portalActive = true
+    //
+  //  }
 }
