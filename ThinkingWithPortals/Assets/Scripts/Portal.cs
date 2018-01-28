0202290114460f4e9a24e763dc412f68959b8559
JackQuick <jack@startvr.co>
2018-01-29 10:14:13 +1100

    Portal glitchyness removed

--- ThinkingWithPortals/Assets/Scripts/Portal.cs
+++ ThinkingWithPortals/Assets/Scripts/Portal.cs
@@ -6,9 +6,8 @@ public class Portal : MonoBehaviour {
 	[Header("Cameras")]
 	public Camera mainCam;
 	public Camera portalCam;
-   // public string arworld1;
-   // public float coolDown = 1.0f;
-   // private bool portalActive = true;
+
+
 
 	void Update () {
 
@@ -18,22 +17,22 @@ public class Portal : MonoBehaviour {
 
 		// Make portal cam face the same direction as the main camera.
 		portalCam.transform.rotation = Quaternion.LookRotation(mainCam.transform.forward, Vector3.up);
+
+        // create a boolean condition around portal entry, then call the 'non rotating' camera whenever we're outside the portal.
+
+        //convert main camera transform from quaternion to eulerangles
+
+        //use the eulerangles rotation to position the portalcam, but set the  z axis to zero to prevent the portal image from rotating
+       // mainCam.transform.rotation.eulerAngles = new Vector3(0, 0, 0);
 	}
 
 	void OnTriggerEnter (Collider other) {
 		if (other.CompareTag ("MainCamera")) {
             // Use xor operator to toggle the ARWorld layer in the mainCam's culling mask.
             mainCam.cullingMask ^= 1 << LayerMask.NameToLayer("ARWorld");
-            //portalCam.cullingMask ^= 1 << LayerMask.NameToLayer(arworld1);
-           // portalActive = false;
-           // StartCoroutine(CoolDown());
+
 		}
 	}
 
-  //  IEnumerator CoolDown()
-  //  {
-  //     yield return new WaitForSeconds(coolDown);
-  //     portalActive = true
-    //
-  //  }
+
 }
